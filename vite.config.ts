601b16778eeb1de9c05881868e5167961f25b8b7
R0b0Vlad15 <vladik.delvalle@gmail.com>
2026-03-29 08:41:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import { ChemicalVitePlugin } from "chemicaljs";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
-@ts-ignore
+//@ts-ignore
 import { generateFile } from "./src/util/generateFileVite";
-@ts-ignore
+//@ts-ignore
 import { generateThemes } from "./src/util/generateThemes";
 import themes from "./src/themes.json";
 
